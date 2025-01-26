5d7fc27d554e6f210e41f2fd815a5e44001823cc
Lorraine Opiyo <achienglorraine8@gmail.com>
2025-01-27 00:07:05 +0300

    updated config.js

--- _config.js
+++ _config.js
@@ -6,4 +6,5 @@ config.mongoURI = {
     development: 'mongodb+srv://lorraine123:lorraine123@cluster0.b6r82.mongodb.net/darkroom?retryWrites=true&w=majority',
     test: 'mongodb+srv://lorraine123:lorraine123@cluster0.b6r82.mongodb.net/darkroom?retryWrites=true&w=majority',
 }
+
 module.exports = config;
